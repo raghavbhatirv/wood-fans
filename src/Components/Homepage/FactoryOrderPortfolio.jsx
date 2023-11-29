6848a7437f2120c5b74d406f70133f108a4cf502
Narayan Das <workmail.narayan@gmail.com>
2023-11-30 02:19:00 +0530

    Homepage Factory order portfolio section

---
+++ src/Components/Homepage/FactoryOrderPortfolio.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import CurvePouf from "./assets/media/Curve pouf.webp";
+import bed from "./assets/media/BED WITH ADJUSTABLE HEADBOARD.webp";
+function FactoryOrderPortfolio() {
+  return (
+    <div className="bg-light-gray px-5">
+      <div className="py-10 flex justify-between ">
+        <h3 className="text-dark text-3xl md:text-4xl">
+          Factory order portfolio
+        </h3>
+        <p className="underline text-sm font-normal">SEE ALL</p>
+      </div>
+      <div class="flex flex-col md:flex-row gap-8">
+        <div>
+          <img src={CurvePouf} />
+          <p className="text-xl py-3 md:py-2">CHILDREN'S BED SOFTY "SOFT"</p>
+          <p className="text-xs font-base text-gray-400">
+            Beautiful bed for a teenager "Softy"
+          </p>
+        </div>
+        <div>
+          <img src={bed} />
+          <p className="text-xl py-2">VELOR SOFA</p>
+          <p className="text-xs font-base text-gray-400">
+            Sofa design artbabaynts
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FactoryOrderPortfolio;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "../Components/Homepage/Hero";
+import BuyersChoice from "../Components/Homepage/BuyersChoice";
+import FactoryOrderPortfolio from "../Components/Homepage/FactoryOrderPortfolio";
 
 function Home() {
   return (
     <div>
       <Hero />
+      {/* <BuyersChoice /> */}
+      <FactoryOrderPortfolio />
     </div>
   );
 }
