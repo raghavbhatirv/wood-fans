a2d96489e025916919bc6e67e29e94a3a104a082
Narayan Das <workmail.narayan@gmail.com>
2023-12-02 02:17:42 +0530

    redux setup for product data.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "../Components/Homepage/Hero";
 import BuyersChoice from "../Components/Homepage/BuyersChoice";
 import FactoryOrderPortfolio from "../Components/Homepage/FactoryOrderPortfolio";
 import OurTeam from "../Components/Homepage/OurTeam";
-
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../Redux/Products/Action";
 function Home() {
+  const { productData } = useSelector((store) => store.dataReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
   return (
     <div>
       <Hero />
---
+++ src/Redux/Products/action.js
@@ -0,0 +1,21 @@
+import { storeDB, query, collection, getDocs } from '../../Services/firebaseConfig'
+import { DATA_GET_REQUEST, DATA_GET_SUCCESS, DATA_GET_FAILURE } from './actionTypes';
+
+export const getDataRequest = () => ({ type: DATA_GET_REQUEST });
+export const getDataSuccess = (data) => ({ type: DATA_GET_SUCCESS, payload: data });
+export const getDataFailure = (error) => ({ type: DATA_GET_FAILURE, payload: error });
+
+export const fetchData = () => async (dispatch) => {
+    dispatch(getDataRequest());
+    try {
+        let tempData = [];
+        const q = query(collection(storeDB, "products"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((product) => {
+            tempData.push({ ...product.data(), id: product.id });
+        });
+        dispatch(getDataSuccess(tempData));
+    } catch (error) {
+        dispatch(getDataFailure(error));
+    }
+};
