119ea43305b0714d3f95e8e5ed25eed77670604c
swatii23 <swati.m_2000@rediffmail.com>
2023-12-03 00:44:38 +0530

    product

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -1,56 +1,65 @@
 import React, { useEffect, useState } from 'react'
-import axios from "axios";
 import { Link } from "react-router-dom";
-import styles from "./productContainer.module.css"
 import { useDispatch, useSelector } from 'react-redux';
-import { store } from '../../Redux/store';
-// import { AddToCart } from "../../Redux/productReducer/action";
+import { fetchData } from '../../Redux/Products/Action';
+import Button from '../Common/Button';
+import load from "./loading.gif"
+
 
 const ProductPage = () => {
-    const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState([])
     const dispatch = useDispatch()
 
-    const productType = useSelector(store => store.productReducer.productType)
-    console.log(productType)
+    const productData = useSelector(store => store.dataReducer.productData)
+    const loading = useSelector(store => store.dataReducer.loading)
+    const [category, setCategory] = useState("sofas")
+    console.log(productData)
 
     
     useEffect(() => {
-    //     dispatch(getData(productType))
-    setProducts([{
-        "id": 1,
-        "title":"DAVE SOFA",
-        "type":"Sofas",
-        "image": "https://api.woodfans.ru/storage/uploads/images/x9V57EeEsrvL9zWFSIUSK1766CK76PQ93XjrLDw1_widened_900.jpg",
-        "url1": "https://api.woodfans.ru/storage/uploads/images/CP65GN3NT3mxIemo9wXMFbJ19sPBXfhe0F9Ivr81_widened_900.jpg",
-        "url2": "https://api.woodfans.ru/storage/uploads/images/ESkUCJSMHzb62y1NeOkuTIEezKsjx0QzHOBb5Bd1_widened_900.jpg",
-        "url3": "https://api.woodfans.ru/storage/uploads/images/M83cTDULNmEr8rJ2qaOouPQjhpirGNXWStoLOKTG_widened_900.jpg",
-        "price": 252000
-      }])
-    }, [productType])
+        dispatch(fetchData())
+    }, [])
+
+    const categoryData = productData.filter(product => {
+      return product.category.toLowerCase() === category.toLowerCase()
+    })
+    console.log(categoryData)
+
+    const redirectToDetail = (id) => {
+      
+    }
     
     if (loading) {
         return (
-          <div className={styles.loading}><div><img src={load} alt="" /></div></div>
+          <div className="py-20 lg:h-65vh flex items-center justify-center"><div><img src={load} alt="" /></div></div>
         )
       }
 
-    return (<div className={styles.container}>
-        <h1>{data.type}</h1>
-        <div className={styles.singleCard}>
+    return (
+    <div className="font-bold font-franklin lg:px-20 lg:py-10 px-5">
+        <h1 className="text-2xl lg:py-5">{category.toUpperCase()}</h1>
+        <div className="grid grid-cols-3 gap-5">
           {
-            products.map((product) => (<div key={product.id}>
-              <img src={product.image} alt="" />
+            categoryData.map((product) => (
+            <div key={product.id} className="shadow-md overflow-hidden" >
+              <div onClick={() => redirectToDetail(product.id)}>
+              <div className="relative" >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.0} stroke="white" className="absolute top-1 right-1 w-10 h-8 p-1 text-white  cursor-pointer">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+             </svg>
+
+              
+            <img src={product.images[0]} alt="" className="w-full lg:h-3/5 object-cover" />
   
-              <div className={styles.nameDiv}>
-                <h4>{product.name}{product.title}</h4>
-                <p>RS {product.price}</p>
               </div>
-              <div className={styles.priceDiv}>
-                <p style={{ "marginTop": "0em" }}>  <Link to={`/product`}>More details</Link></p>
-                <p> {product.cost}</p>
-                <button onClick={() => { dispatch(AddToCart([{ ...product, qty: 1 }])) }}>Add to Cart
-                </button>
+              <div className="p-2 lg:p-5 flex lg:gap-40 gap-5 md:gap-20">
+                <h4 className="lg:font-semibold font-normal lg:px-5 text-xs lg:text-base" >{product.name}</h4>
+                <p className="text-gray-700 text-xs lg:text-base md:pl-5" >RS {product.price}</p>
+              </div>
+              </div>
+              <div className="flex justify-between lg:p-4 p-0">
+                {/* <button onClick={() => { dispatch(AddToCart([{ ...product, qty: 1 }])) }}>Add to Cart
+                </button> */}
+                <Button text={"Add to Cart"} className="bg-gray-800 text-white px-1 py-2 rounded-md hover:bg-gray-700 text-xs lg:px-4 py-2" />
               </div>
             </div>
             ))
