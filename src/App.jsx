c47166af564910ff9eef864215c9b7eebc37dec1
Raghavbhati <bhatiraghav4@gmail.com>
2023-12-03 01:25:32 +0530

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Common/Navbar/Navbar";
+import Footer from "./Components/Common/Footer"
+import Home from "./Pages/Home";
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+   
+export default App;
