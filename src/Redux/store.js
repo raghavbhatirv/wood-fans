61d5647966739a055253548ceece49b68dd070c1
swatii23 <swati.m_2000@rediffmail.com>
2023-12-02 16:00:37 +0530

    product

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,7 @@
 import { legacy_createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import productReducer from "./ProductReducer/productReducer";
 
 
 
@@ -20,7 +21,7 @@ const reducer =(state=initalState,action)=>{
 
 // You can add your own reducer in the combineReducers
 
-const rootReducer = combineReducers({reducer})
+const rootReducer = combineReducers({reducer, productReducer})
 
 const store = legacy_createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
