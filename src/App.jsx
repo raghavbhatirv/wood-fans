1535e6b135ac148bee73bd104094b9bee2780eca
Raghavbhati <bhatiraghav4@gmail.com>
2023-12-03 13:55:15 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,6 @@
-
-import Navbar from "./Components/Common/Navbar/Navbar";
-import Footer from "./Components/Common/Footer"
-import Home from "./Pages/Home";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-import Signup from "./Pages/SignUp&Login/Signup";
-import LoginAndSignup from "./Pages/SignUp&Login/LoginAndSignup";
-import Demo from "./Components/Common/Demo";
-import { useDispatch, useSelector } from "react-redux";
-import { toggleTheme } from "./Redux/Theme/action";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useEffect } from "react";
-import { loginWithEmailAndPassword } from "./Redux/Auth/action";
-import Footer from "./Components/Common/Footer";
-import SingleProduct from "./Pages/SingleProduct";
+import Navbar from "./Components/Common/Navbar/Navbar"
 
 function App() {
-  const dispatch = useDispatch();
-
-  const theme = useSelector((store) => store.themeReducer.theme);
-
-  const handleTheme = () => {
-    dispatch(toggleTheme());
-  };
-
-  // Remember Me functionalites
-  /*
-  useEffect(() => {
-    const rememberMe = localStorage.getItem("rememberMeUser");
-    if (rememberMe) {
-      const { email, password } = JSON.parse(rememberMe);
-      dispatch(loginWithEmailAndPassword(email, password));
-    }
-  }, [dispatch]);
-  */
-
   return (
     <>
       <div className="font-Poppins">
@@ -44,5 +9,5 @@ function App() {
     </>
   );
 }
-   
+
 export default App;
--- src/Components/Common/Navbar/Navbar.jsx
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import styles from "./Navbar.module.css";
 import logo from "../../../assets/logo.svg";
+import { Link, useNavigate } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux";
 
 const Navbar = () => {
+    const category = useSelector((store) => store.dataReducer);
+    console.log(category)
+    const dispatch = useDispatch();
+    const navigate = useNavigate()
     const [search, setSearch] = useState("")
     const [isMobileMenuActive, setMobileMenuActive] = useState(false);
+    const [cartValue, setCartValue] = useState(0);
     const toggleMobileMenu = () => {
         setMobileMenuActive(!isMobileMenuActive);
     };
@@ -13,9 +20,11 @@ const Navbar = () => {
 
     }
     const toHome = () => {
-
+        navigate("/")
     }
-
+    const categoryChangeInStore = (event, value) => {
+        console.log(value);
+    };
     return (
         <div>
             <div id={styles["navbar-id"]} className="max-sm:bg-gray-200">
@@ -28,11 +37,11 @@ const Navbar = () => {
                             <div className="flex justify-between items-center">
                                 <div>
                                     <ul className="list-none flex max-lg:hidden">
-                                        <a href="#"><li className="px-4 py-2 text-base uppercase hover:text-primary-yellow">About Productions</li></a>
-                                        <a href="#"><li className="px-4 py-2 text-base uppercase hover:text-primary-yellow">Shipping & Payment</li></a>
-                                        <a href="#"><li className="px-4 py-2 text-base uppercase hover:text-primary-yellow" >blog</li></a>
-                                        <a href="#"><li className="px-4 py-2 text-base uppercase hover:text-primary-yellow">Portfolio</li></a>
-                                        <a href="#"><li className="px-4 py-2 text-base uppercase hover:text-primary-yellow">Contacts</li></a>
+                                        <a href="/"><li className="px-4 py-2 text-base uppercase hover:text-primary-yellow">About Productions</li></a>
+                                        <a href="/"><li className="px-4 py-2 text-base uppercase hover:text-primary-yellow">Shipping & Payment</li></a>
+                                        <a href="/"><li className="px-4 py-2 text-base uppercase hover:text-primary-yellow" >blog</li></a>
+                                        <a href="/"><li className="px-4 py-2 text-base uppercase hover:text-primary-yellow">Portfolio</li></a>
+                                        <a href="/"><li className="px-4 py-2 text-base uppercase hover:text-primary-yellow">Contacts</li></a>
                                     </ul>
                                 </div>
                                 <div className="flex justify-between items-center gap-3.5">
@@ -41,11 +50,12 @@ const Navbar = () => {
                                         <a onClick={searchData} href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
                                     </div>
                                     <div className="max-sm:hidden">
-                                        <a href="#"><p className="text-sm font-bold max-sm:text-base">Login/SignUp</p></a>
+                                        <Link to="/login"><p className="text-sm font-bold max-sm:text-base">Login/SignUp</p></Link>
                                     </div>
                                     <div className="flex items-center justify-between gap-5">
                                         <div className="relative">
-                                            <a href="#"><i className="fa-solid fa-bag-shopping max-sm:text-3xl text-3xl"></i></a>
+                                            <Link to="/cart"><i className="fa-solid fa-bag-shopping max-sm:text-3xl text-3xl"></i></Link>
+                                            <p className="text-white text-xs absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-2">{cartValue}</p>
                                         </div>
                                         <div className="lg:hidden">
                                             {isMobileMenuActive ? <a onClick={toggleMobileMenu} ><i className="fa-solid fa-xmark text-3xl"></i></a> : <a onClick={toggleMobileMenu} ><i className="fa-solid fa-bars text-3xl"></i></a>}
@@ -58,11 +68,11 @@ const Navbar = () => {
                             <div className="flex justify-between items-center">
                                 <div className=" max-lg:invisible">
                                     <ul className="list-none flex">
-                                        <a href="#"><li className="px-4 py-2 text-base hover:opacity-50">Sofa's</li></a>
-                                        <a href="#"><li className="px-4 py-2 text-base hover:opacity-50">Beds</li></a>
-                                        <a href="#"><li className="px-4 py-2 text-base hover:opacity-50">Children furniture</li></a>
-                                        <a href="#"><li className="px-4 py-2 text-base hover:opacity-50">Chairs</li></a>
-                                        <a href="#"><li className="px-4 py-2 text-base hover:opacity-50">Standards</li></a>
+                                        <Link onClick={(event) => categoryChangeInStore(event, "SOFAS")} to="/products"><li className="px-4 py-2 text-base hover:opacity-50">Sofa's</li></Link>
+                                        <Link onClick={(event) => categoryChangeInStore(event, "BEDS")} to="/products"><li className="px-4 py-2 text-base hover:opacity-50">Beds</li></Link>
+                                        <Link onClick={(event) => categoryChangeInStore(event, "CHILDREN'S FURNITURE")} to="/products"><li className="px-4 py-2 text-base hover:opacity-50">Children furniture</li></Link>
+                                        <Link onClick={(event) => categoryChangeInStore(event, "ARMCHAIRS AND POUFS")} to="/products"><li className="px-4 py-2 text-base hover:opacity-50">Chairs</li></Link>
+                                        <Link onClick={(event) => categoryChangeInStore(event, null)} to="/products"><li className="px-4 py-2 text-base hover:opacity-50">Standards</li></Link>
                                     </ul>
                                 </div>
                                 <div className="text-right">
@@ -74,22 +84,23 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
+            {/* Mobile Menu */}
             <div className={`${isMobileMenuActive ? styles.mobileMenu_active : styles.mobile_menu} hidden max-lg:block`} >
                 <div className="p-4">
                     <div>
                         <ul className="list-none">
-                            <a href="#"><li className="px-4 py-2 uppercase text-white text-2xl">Sofa's</li></a>
-                            <a href="#"><li className="px-4 py-2 uppercase  text-white text-2xl">Beds</li></a>
-                            <a href="#"><li className="px-4 py-2 uppercase  text-white text-2xl">Children furniture</li></a>
-                            <a href="#"><li className="px-4 py-2 uppercase  text-white text-2xl">Chairs</li></a>
-                            <a href="#"><li className="px-4 py-2 uppercase  text-white text-2xl">Standards</li></a>
+                            <Link onClick={(event) => categoryChangeInStore(event, "SOFAS")} to="/products"><li className="px-4 py-2 uppercase text-white text-2xl">Sofa's</li></Link>
+                            <Link onClick={(event) => categoryChangeInStore(event, "BEDS")} to="/products"><li className="px-4 py-2 uppercase  text-white text-2xl">Beds</li></Link>
+                            <Link onClick={(event) => categoryChangeInStore(event, "CHILDREN'S FURNITURE")} to="/products"><li className="px-4 py-2 uppercase  text-white text-2xl">Children furniture</li></Link>
+                            <Link onClick={(event) => categoryChangeInStore(event, "ARMCHAIRS AND POUFS")} to="/products"><li className="px-4 py-2 uppercase  text-white text-2xl">Chairs</li></Link>
+                            <Link onClick={(event) => categoryChangeInStore(event, null)} to="/products"><li className="px-4 py-2 uppercase  text-white text-2xl">Standards</li></Link>
                         </ul>
                         <ul className="list-none">
-                            <a href="#"><li className="px-4 py-2 font-medium text-sm">Shipping & Payment</li></a>
-                            <a href="#"><li className="px-4 py-2 font-medium text-sm">About Productions</li></a>
-                            <a href="#"><li className="px-4 py-2 font-medium text-sm">Blog</li></a>
-                            <a href="#"><li className="px-4 py-2 font-medium text-sm">Portfolio</li></a>
-                            <a href="#"><li className="px-4 py-2 font-medium text-sm">Contacts</li></a>
+                            <a href="/"><li className="px-4 py-2 font-medium text-sm">Shipping & Payment</li></a>
+                            <a href="/"><li className="px-4 py-2 font-medium text-sm">About Productions</li></a>
+                            <a href="/"><li className="px-4 py-2 font-medium text-sm">Blog</li></a>
+                            <a href="/"><li className="px-4 py-2 font-medium text-sm">Portfolio</li></a>
+                            <a href="/"><li className="px-4 py-2 font-medium text-sm">Contacts</li></a>
                         </ul>
                     </div>
                     <div>
